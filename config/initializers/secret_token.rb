8e0887871cb10e67470b605b7e7f1b8b8ce744ad
James Edward Gray II <james@graysoftinc.com>
2013-08-14 16:11:09 -0500

    The initial Rails application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OklahomaatheistsCom::Application.config.secret_key_base = 'f50abb397efd7cc93960bc06ab513d4dee1c3b7671454f6ce35233096f2e5db8959fea560d7e86e4526b0b88a1cda7ba3772a12063d6acbe1388a520912e2453'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OklahomaatheistsCom::Application.config.session_store :cookie_store, key: '_oklahomaatheists_com_session'
