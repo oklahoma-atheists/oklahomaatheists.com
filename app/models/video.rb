7fe62c410e99a203c538b4e192043d1a41ec5428
James Edward Gray II <james@graysoftinc.com>
2015-02-02 20:36:26 -0600

    Disabling bad account.

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,7 @@
 class Video
   def self.recent
+    return [ ]
+
     AOK::APIs.youtube.refresh_access_token!
     videos = AOK::APIs.youtube
                       .videos_by(user: ENV.fetch("YOUTUBE_USERNAME"))
